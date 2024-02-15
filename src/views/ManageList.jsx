71b06a43f8f11b5fc2e5cc38c26c53cc22766476
Ricky Barfield <barfieldfredrick@gmail.com>
2024-02-15 08:35:28 -0700

    added in some of Britts suggestions regarding reminder names

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -4,7 +4,9 @@ import { useState } from 'react';
 export function ManageList({ listPath }) {
 	const [message, setMessage] = useState('');
 	const [userItem, setUserItem] = useState('');
-	const [itemDuration, setItemDuration] = useState('');
+	// Presetting item duration to 7, as that option starts
+	// selected on page rendering.
+	const [itemDuration, setItemDuration] = useState(7);
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -37,14 +39,15 @@ export function ManageList({ listPath }) {
 						value={userItem}
 					/>
 				</div>
+				<span>Remind me to replace item in...</span> <br />
 				<select
-					onChange={(e) => setItemDuration(Number(e.target.value))}
+					onChange={(e) => setItemDuration(e.target.value)}
 					name="replaceTime"
 					value={itemDuration}
 				>
-					<option value={'7'}>Soon</option>
-					<option value={'14'}>Kind of soon</option>
-					<option value={'30'}>Not Soon</option>
+					<option value={7}>7 days</option>
+					<option value={14}>14 days</option>
+					<option value={30}>30 days</option>
 				</select>
 				<div>
 					<button>Submit</button>
