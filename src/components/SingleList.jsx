c8e9e3b02b2d500e40592620bf6973433aa47b41
Britt Danzer <britt.danzer@gmail.com>
2024-02-14 20:22:53 -0600

    remove

--- src/components/SingleList.jsx
+++ src/components/SingleList.jsx
@@ -1,7 +1,6 @@
 import './SingleList.css';
 
 export function SingleList({ name, path, setListPath }) {
-	console.log({ name, path, setListPath });
 	function handleClick() {
 		setListPath(path);
 	}
