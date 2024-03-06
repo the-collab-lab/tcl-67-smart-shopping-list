2b3df5db1b14e7c9234567057dff5c7c024cd1b5
Britt Danzer <britt.danzer@gmail.com>
2024-03-05 19:07:37 -0600

    add comment

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import { App } from '../src/App';
 
+// Example test with RTL that is here
 it('renders without crashing', () => {
 	render(<App />);
 });
