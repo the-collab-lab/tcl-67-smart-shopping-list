148908b6743e46c0592ae9f0c0c6f8f0253801ca
Ricky Barfield <barfieldfredrick@gmail.com>
2024-03-14 21:18:59 -0700

    trying to do this with a Map function, as it can track dates between as a value

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,4 +1,5 @@
 import { calculateEstimate } from '@the-collab-lab/shopping-list-utils';
+import { act } from 'react-dom/test-utils';
 
 let ONE_DAY_IN_MILLISECONDS = 86400000;
 
@@ -66,40 +67,91 @@ export function getNextPurchasedDate({
 }
 
 export function sortByDaysBetweenDates(data) {
-	let activeArr = [];
 	let inactiveArr = [];
+	const activeMap = new Map();
 
 	data.forEach((item) => {
-		if (
-			getDaysBetweenDates(
-				item.dateLastPurchased?.toDate(),
-				item.dateNextPurchased?.toDate(),
-			) > 60
-		) {
+		const daysBetween = getDaysBetweenDates(
+			item.dateLastPurchased?.toDate(),
+			item.dateNextPurchased?.toDate(),
+		);
+
+		if (daysBetween > 60) {
 			inactiveArr.push(item);
 		} else {
-			activeArr.push(item);
+			activeMap.set(item, daysBetween);
 		}
 	});
 
-	activeArr.sort((a, b) => {
-		const numOfDaysA = getDaysBetweenDates(
-			a.dateLastPurchased?.toDate(),
-			a.dateNextPurchased?.toDate(),
-		);
-		const numOfDaysB = getDaysBetweenDates(
-			b.dateLastPurchased?.toDate(),
-			b.dateNextPurchased?.toDate(),
-		);
+	// Convert activeMap to an array of key-value pairs and sort it by the number of days between dates
+	const sortedActiveArray = Array.from(activeMap).sort((a, b) => a[1] - b[1]);
 
-		if (numOfDaysA < numOfDaysB) {
-			return -1;
-		}
-		if (numOfDaysA > numOfDaysB) {
-			return 1;
-		}
-		return 0;
-	});
+	// Reconstruct activeMap from the sorted array
+	const sortedActiveMap = new Map(sortedActiveArray);
 
-	return [...activeArr, ...inactiveArr];
+	console.log(...sortedActiveMap.values());
+	return [...sortedActiveMap.keys(), ...inactiveArr];
 }
+
+// export function sortByDaysBetweenDates(data) {
+// 	// let activeArr = [];
+// 	let inactiveArr = [];
+// 	const activeMap = new Map();
+// 	let reorderedItems = [];
+// 	let currentIndex = 0;
+// 	let lowestNum = 0;
+// 	// const inActiveMap = new Map();
+
+// 	data.forEach((item) => {
+// 		if (
+// 			getDaysBetweenDates(
+// 				item.dateLastPurchased?.toDate(),
+// 				item.dateNextPurchased?.toDate(),
+// 			) > 60
+// 		) {
+// 			inactiveArr.push(item);
+// 		} else {
+// 			activeMap.set(
+// 				item,
+// 				getDaysBetweenDates(
+// 					item.dateLastPurchased?.toDate(),
+// 					item.dateNextPurchased?.toDate(),
+// 				),
+// 			);
+// 		}
+// 	});
+
+// 	for (let [key, value] of activeMap) {
+// 		for (let [key2, value2] of activeMap) {
+// 			if (value2 === lowestNum) {
+// 				continue;
+// 			} else if (value2 < lowestNum) {
+// 				lowestNum = value2;
+// 			}
+// 		}
+// 		// console.log(key.name);
+// 	}
+
+// 	console.log(activeMap.values());
+
+// 	// activeArr.sort((a, b) => {
+// 	// 	const numOfDaysA = getDaysBetweenDates(
+// 	// 		a.dateLastPurchased?.toDate(),
+// 	// 		a.dateNextPurchased?.toDate(),
+// 	// 	);
+// 	// 	const numOfDaysB = getDaysBetweenDates(
+// 	// 		b.dateLastPurchased?.toDate(),
+// 	// 		b.dateNextPurchased?.toDate(),
+// 	// 	);
+
+// 	// 	if (numOfDaysA < numOfDaysB) {
+// 	// 		return -1;
+// 	// 	}
+// 	// 	if (numOfDaysA > numOfDaysB) {
+// 	// 		return 1;
+// 	// 	}
+// 	// 	return 0;
+// 	// });
+
+// 	return [...activeMap.keys(), ...inactiveArr];
+// }
