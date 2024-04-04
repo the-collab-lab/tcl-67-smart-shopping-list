5cdf0a4f15b362f97d46ed2377f0598b7b6c9ad4
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2024-04-04 10:42:52 -0500

    create svg

--- src/components/LoggedOut.jsx
+++ src/components/LoggedOut.jsx
@@ -5,7 +5,51 @@ export default function LoggedOut() {
 				<h2>Log in to view your lists.</h2>
 				<h2>Plan your next grocery run with smart shopping list.</h2>
 			</div>
-			<div>IMAGE PLACEHOLDER</div>
+			<div>
+				<svg width="100%" height="100%">
+					<rect width="100%" height="100%" fill="#fdfd18" />
+					<line
+						x1="0"
+						y1="20%"
+						x2="20%"
+						y2="0"
+						stroke="#dcff4b"
+						style={{ strokeWidth: '50px' }}
+					/>
+					<line
+						x1="0"
+						y1="60%"
+						x2="60%"
+						y2="0"
+						stroke="#dcff4b"
+						style={{ strokeWidth: '15px' }}
+					/>
+					<line
+						x1="0"
+						y1="100%"
+						x2="100%"
+						y2="0"
+						stroke="#dcff4b"
+						style={{ strokeWidth: '15px' }}
+					/>
+					<line
+						x1="40%"
+						y1="100%"
+						x2="100%"
+						y2="40%"
+						stroke="#dcff4b"
+						style={{ strokeWidth: '15px' }}
+					/>
+					<line
+						x1="80%"
+						y1="100%"
+						x2="100%"
+						y2="80%"
+						stroke="#dcff4b"
+						style={{ strokeWidth: '15px' }}
+					/>
+				</svg>
+			</div>
 		</section>
 	);
 }
