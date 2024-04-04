5f7fbcfb8d19afd5261a48e7a184920cc0f49892
Ricky Barfield <barfieldfredrick@gmail.com>
2024-04-04 08:03:08 -0700

    footer button now moves on small screen sizes at initial start

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -4,9 +4,20 @@ import './Layout.css';
 import { auth } from '../api/config.js';
 import { useAuth, SignInButton, SignOutButton } from '../api/useAuth.jsx';
 import LoggedOut from '../components/LoggedOut.jsx';
+import { useEffect, useState } from 'react';
 
 export function Layout() {
 	const { user } = useAuth();
+	const [isMobile, setIsMobile] = useState();
+
+	useEffect(() => {
+		if (window.innerWidth < 780) {
+			setIsMobile(true);
+		} else {
+			setIsMobile(false);
+		}
+	}, []);
+
 	return (
 		<div className="Layout">
 			{!!user ? (
@@ -16,24 +27,40 @@ export function Layout() {
 							<h1>Smart shopping list</h1>
 						</a>
 
-						<div>
-							<span>Welcome, {auth.currentUser.displayName}</span>
-						</div>
-						<SignOutButton />
+						{!isMobile && (
+							<div>
+								<div>
+									<span>Welcome, {auth.currentUser.displayName}</span>
+								</div>
+								<SignOutButton />
+							</div>
+						)}
 					</header>
 					<main className="Layout-main">
 						<Outlet />
 					</main>
+					{isMobile && (
+						<div className="mobileLogFooter">
+							<SignOutButton />
+						</div>
+					)}
 				</>
 			) : (
 				<>
 					<header className="Layout-header">
 						<h1>Smart shopping list</h1>
-						<div>
-							<SignInButton />
-						</div>
+						{!isMobile && (
+							<div>
+								<SignInButton />
+							</div>
+						)}
 					</header>
 					<LoggedOut />
+					{isMobile && (
+						<div className="mobileLogFooter">
+							<SignInButton />
+						</div>
+					)}
 				</>
 			)}
 		</div>
