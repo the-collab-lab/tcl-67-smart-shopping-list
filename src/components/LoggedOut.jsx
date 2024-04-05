a84fcff967678bdc9b7df96a997b616f0dbe215a
Jenny Takahara <jennytakahara@gmail.com>
2024-04-04 22:16:41 -0600

    adding css file for LoggedOut component

--- src/components/LoggedOut.jsx
+++ src/components/LoggedOut.jsx
@@ -1,11 +1,12 @@
+import './LoggedOut.css';
 export default function LoggedOut() {
 	return (
 		<section className="sideBySide-loggedOut">
-			<div>
+			<div className="loggedOut-message">
 				<h2>Plan your next grocery run with CleverCart.</h2>
 				<h4>Log in to view your lists.</h4>
 			</div>
-			<div>IMAGE PLACEHOLDER</div>
+			<div className="loggedOut-image">IMAGE PLACEHOLDER</div>
 		</section>
 	);
 }
