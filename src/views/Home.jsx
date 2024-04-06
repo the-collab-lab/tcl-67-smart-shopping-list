3074976d13b44b67aa117cb87fa383b0af1fbf2c
Fredrick Barfield <barfieldfredrick@gmail.com>
2024-04-05 18:41:23 -0700

    home page padding and borders set

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -12,13 +12,15 @@ export function Home({
 	return (
 		<div className="Home">
 			{!listPath ? (
-				<h2>No list currently selected!</h2>
+				<div className="welcomeArea">
+					<h2>No list currently selected!</h2>
+				</div>
 			) : (
-				<>
+				<div className="welcomeArea">
 					<h4>Welcome, {auth.currentUser.displayName}</h4>
 					<small>Current List:</small>
 					<h5>{listName}</h5>
-				</>
+				</div>
 			)}
 
 			<AddListForm setListPath={setListPath} />
